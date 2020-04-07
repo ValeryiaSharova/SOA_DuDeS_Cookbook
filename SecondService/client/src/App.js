75a902379c8e40f51b0739e7ea43acf5633a84d0
Aller-Forst <gominzyplay@gmail.com>
2020-04-07 20:59:06 +0300

    Routing was implemented.

--- SecondService/client/src/App.js
+++ SecondService/client/src/App.js
@@ -1,4 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { ModalProvider } from './context/ModalContext';
+import ModalRoot from './context/ModalRoot';
+import Header from './sharedComponents/Header/Header';
+import Page from './pages/Index/IndexPage';
+import Ingredients from './pages/Ingredients/IngredientsPage';
 
-const App = () => <div>Hello, React</div>;
+const App = () => (
+  <BrowserRouter>
+    <ModalProvider>
+      <ModalRoot />
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Page} />
+        <Route path="/ingredients" exact component={Ingredients} />
+      </Switch>
+    </ModalProvider>
+  </BrowserRouter>
+);
 export default App;
