a23763dd3b4268fd1a7956c82a507d7f7fc0284f
nikita.tsvirko <tsvirko.nikita@gmail.com>
2020-04-13 00:20:49 +0300

    Implemented Repository project

---
+++ Cookbook.Authentication/Cookbook.Authentication.Repository/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Cookbook.Authentication.Repository.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cookbook.Authentication.Repository
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
