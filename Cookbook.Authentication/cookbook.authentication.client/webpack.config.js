a3183aec1aea68e10cac4d5839e793073f97b355
nikita.tsvirko <tsvirko.nikita@gmail.com>
2020-04-13 23:31:21 +0300

    Initialized React app

---
+++ Cookbook.Authentication/cookbook.authentication.client/webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+        '@': path.resolve(__dirname, 'src/'),
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ],
+  devServer: {
+    historyApiFallback: true
+  },
+  externals: {
+    config: JSON.stringify({
+        apiUrl: 'http://localhost:2000/api'
+    })
+  }
+};
