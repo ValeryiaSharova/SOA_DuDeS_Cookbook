64765a60c526c44fa001e67b71211d41d713c58d
nikita.tsvirko <tsvirko.nikita@gmail.com>
2020-04-13 12:27:02 +0300

    Implemented UserService

---
+++ Cookbook.Authentication/Cookbook.Authentication.Service/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Cookbook.Authentication.Repository.Models;
+using System.Collections.Generic;
+
+namespace Cookbook.Authentication.Service.Interfaces
+{
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+        User Create(User user, string password);
+        void Update(User user, string password = null);
+        void Delete(int id);
+        string GenerateToken(User user, string secret);
+        bool ValidateToken(string token, string secret);
+    }
+}
