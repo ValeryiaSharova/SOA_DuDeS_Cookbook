2a6e0ad847f8eb76fdcfe0928ad215dac08898f0
Aller-Forst <gominzyplay@gmail.com>
2020-04-14 22:51:24 +0300

    Implemented redux store.

--- IngredientsService/client/src/App.js
+++ IngredientsService/client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ModalProvider } from './context/ModalContext';
+import store from './redux/store';
 import ModalRoot from './context/ModalRoot';
 import Header from './sharedComponents/Header/Header';
 import Page from './pages/Index/IndexPage';
@@ -8,15 +10,17 @@ import Ingredients from './pages/Ingredients/IngredientsPage';
 import PrivateRoute from './sharedComponents/PrivateRoute/PrivateRoute';
 
 const App = () => (
-  <BrowserRouter>
-    <ModalProvider>
-      <ModalRoot />
-      <Header />
-      <Switch>
-        <Route path="/" exact component={Page} />
-        <PrivateRoute path="/ingredients" exact component={Ingredients} />
-      </Switch>
-    </ModalProvider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <ModalProvider>
+        <ModalRoot />
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Page} />
+          <PrivateRoute path="/ingredients" exact component={Ingredients} />
+        </Switch>
+      </ModalProvider>
+    </BrowserRouter>
+  </Provider>
 );
 export default App;
