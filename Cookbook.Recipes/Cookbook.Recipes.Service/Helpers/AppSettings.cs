442708c329d87af3deab5564fc5b1a9fdd4a4608
nikita.tsvirko <tsvirko.nikita@gmail.com>
2020-04-15 01:29:21 +0300

    Implemented AuthorizationService wrapper for AuthorizationAPI

---
+++ Cookbook.Recipes/Cookbook.Recipes.Service/Helpers/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Cookbook.Recipes.Service.Helpers
+{
+    public class AppSettings
+    {
+        public string AuthorizationApiUrl { get; set; }
+        public string IngredientsApiUrl { get; set; }
+    }
+}
