3adb315f325974ce1157701c420a0e31d1e76a7a
nikita.tsvirko <tsvirko.nikita@gmail.com>
2020-04-15 05:00:15 +0300

    Implemented IngredientService wrapper for IngredientAPI

---
+++ Cookbook.Recipes/Cookbook.Recipes.Service/Interfaces/IIngredientService.cs
@@ -0,0 +1,12 @@
+﻿using Cookbook.Recipes.Repository.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cookbook.Recipes.Service.Interfaces
+{
+	public interface IIngredientService
+	{
+		Task<IEnumerable<string>> GetAllIngredientsNameAsync(string accessToken);
+		Task<int> GetIngredientCaloriesByName(Ingredient ingredient, string accessToken);
+	}
+}
