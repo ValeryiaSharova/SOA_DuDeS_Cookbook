10f535ff60c37199737f77f43085e35889190968
nikita.tsvirko <tsvirko.nikita@gmail.com>
2020-04-15 05:00:47 +0300

    Implemeted RecipesService

---
+++ Cookbook.Recipes/Cookbook.Recipes.Service/Interfaces/IRecipesService.cs
@@ -0,0 +1,14 @@
+﻿using Cookbook.Recipes.Repository.Models;
+using System.Collections.Generic;
+
+namespace Cookbook.Recipes.Service.Interfaces
+{
+	public interface IRecipesService
+	{
+		IEnumerable<Recipe> GetAll();
+		Recipe GetById(int id);
+		Recipe Create(Recipe recipe);
+		void Update(Recipe recipe);
+		void Delete(int id);
+	}
+}
